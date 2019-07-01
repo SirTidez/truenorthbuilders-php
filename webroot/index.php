448d2f542287fdfbd073d6ed3e673b90ebe04bc3
Tyler Ludka <itidez@gmail.com>
2019-06-30 22:10:38 -0700

    Init Commit
    Initial commit of the TNB files for public production

--- webroot/index.php
+++ webroot/index.php
@@ -1,40 +1,187 @@
-<?php
-/**
- * The Front Controller for handling every request
- *
- * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
- * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
- *
- * Licensed under The MIT License
- * For full copyright and license information, please see the LICENSE.txt
- * Redistributions of files must retain the above copyright notice.
- *
- * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
- * @link          https://cakephp.org CakePHP(tm) Project
- * @since         0.2.9
- * @license       MIT License (https://opensource.org/licenses/mit-license.php)
- */
+<!DOCTYPE html>
+<html class="no-js" lang="en" dir="ltr">
+    <head>
+        <title>True North Builders</title>
+	<meta charset="UTF-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+        <meta name="description" content="True North Builders" />
+        <meta name="keywords" content="jquery, css3, 3d, webkit, fallback, slider, css3, 3d transforms, slices, rotate, box, automatic" />
+	<meta name="author" content="Tyler Ludka" />
+	<link rel="shortcut icon" href="../favicon.ico"> 
+        <link rel="stylesheet" type="text/css" href="css/demo.css" />
+	<link rel="stylesheet" type="text/css" href="css/slicebox.css" />
+	<link rel="stylesheet" type="text/css" href="css/foundation.css" />
+        <link rel="stylesheet" type="text/css" href="css/styles.css" />
+	<link rel="stylesheet" type="text/css" href="css/custom.css" />
+        <link rel="stylesheet" href="css/app.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/motion-ui@1.2.3/dist/motion-ui.min.css" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+        <link rel="stylesheet" type="text/css" href="css/foundation-icons.css" />
+	<link rel="manifest" href="/site.webmanifest">
+        <link href='http://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
+        <link href="https://fonts.googleapis.com/css?family=Titillium+Web:400,200,300,700,600|Raleway:400,700,300" rel="stylesheet" type="text/css">
+<!--	<script type="text/javascript" src="js/modernizr.custom.46884.js"></script>-->
+	<script src="https://kit.fontawesome.com/a3270dcb61.js"></script>
+	</head>
+	<body>
+                                    <div data-sticky-container data-sticky data-options="marginTop:0;" style="width:100%">
+                                    <div class="grid-container fluid header">
+                                    <div class="grid-x grid-margin-x align-middle">
+                                        <div class="cell large-4">
+                                            <h1><span>Coeur d'Alene Custom Home Builder, Home Remodeling and General Contractor</span></h1>
+                                        </div>
+                                        <div class="cell auto">
+                                            <img src="images/logo_small.png" alt="True North Builders" />
+                                        </div>
+                                        <div class="cell auto">
+                                            <h1><span>Located in Coeur d'Alene, serving Coeur d'Alene and surrounding areas</span></h1>
+                                        </div>
+                                    </div>
+                                    </div>
+                <div class="title-bar" data-responsive-toggle="responsive-menu" data-hide-for="medium">
+                    <button class="menu-icon" type="button" data-toggle="responsive-menu"></button>
+                    <div class="title-bar-title">Menu</div>
+                </div>
 
-// Check platform requirements
-require dirname(__DIR__) . '/config/requirements.php';
+                
+                    <div class="top-bar clearfix" id="responsive-menu" id="nav-menu">
+                            <div class="top-bar-left">
+                                <ul class="dropdown menu align-middle" data-dropdown-menu>
+                                    <li><a href="#home">Home</a>
+                                    <li><a href="remodel.php">Remodeling</a></li>
+                                    <li><a href="#contactus">Contact Us</a></li>
+                                </ul>
+                            </div>
+                    
+                    <div class="top-bar-right">
+                                <ul class="menu">
+                                    <li><button type="button" class="button" data-open="quoteForm">Get a Quote</button></li>
+                                </ul>
+                            </div>
+                </div>
+            </div>
+            
+            <div id="slide1">
+                <div class="slide-content">
+                    <div id="phb">
+                    </div>
+                </div>
+            </div>
+            <div id="slide2">
+                <div class="slide-content">
+                    <div class="slide-text-container">
+                        <h2>Professional Home Building</h2>
+                        <h3 class="slide-text">At True North Builders LLC we are passionate about building homes! From the conception of the idea to your move-in day, we love being there every step of the way helping you realize your dream.
 
-// For built-in server
-if (php_sapi_name() === 'cli-server') {
-    $_SERVER['PHP_SELF'] = '/' . basename(__FILE__);
+Residential construction is an important industry. As builders, we are creating the home in which you’ll be living for many years to come. You will conduct your day-to-day life there as well as celebrate major life events and milestones. In a city that is constantly growing and changing, helping you establish yourself or your family in the landscape is our favorite thing to do! Everyone at True North Builders LLC has a real passion for their work and that comes through in the homes we build.
 
-    $url = parse_url(urldecode($_SERVER['REQUEST_URI']));
-    $file = __DIR__ . $url['path'];
-    if (strpos($url['path'], '..') === false && strpos($url['path'], '.') !== false && is_file($file)) {
-        return false;
-    }
-}
-require dirname(__DIR__) . '/vendor/autoload.php';
+Licensed and experienced, you can rely on our team to make the entire process an enjoyable one.</h3>
+                    </div>
+                </div>
+            </div>
+                                    <div id="slide3">
+                                        <div class="slide-content">
+                                            <div id="bpc">
+                                                
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div id="slide4">
+                                        <div class="slide-content">
+                                            <div class="slide-text-container">
+                                                <h2>Big Picture Commitment</h2>
+                                                <h3 class="slide-text">At a cost of roughly $150 per square foot, building a house from the ground up is a daunting task, and one of the most expensive purchases you can make in your life. Your contractor and builders will likely start feeling like family by the end of the process—and that is exactly the relationship we aim to build with you. True North Builders LLC puts a lot of value in your trust and will always return it in kind.
 
-use App\Application;
-use Cake\Http\Server;
+This is also the relationship we have with our subcontractors. We put a lot of care into our work, and so all our partner companies must have the same standards. When subcontracting is necessary, you can be sure the company is an established one that has done quality work for us in the past. This means you will not have to worry about us hiring an untested subcontractor to install your windows only to result in shoddy workmanship when the rest of your home is up to code!
 
-// Bind your application to the server.
-$server = new Server(new Application(dirname(__DIR__) . '/config'));
+Our contractors look at building a new home as one large puzzle. If all the pieces do not connect properly, they will not present it to you as a completed job.</h3>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div id="slide5">
+                                        <div class="slide-content">
+                                            <div id="gi"></div>
+                                        </div>
+                                    </div>
+                                    <div id="slide6">
+                                        <div class="slide-content">
+                                            <div class="slide-text-container">
+                                                <h2>Get Inspired</h2>
+                                                <h3 class="slide-text">With True North Builders LLC, while it’s an option, you don’t have to be constrained to a pre-made blueprint that may have been used by other homeowners. We encourage you to look through our portfolio and around the Internet, gather your ideas, and together we can design an entirely custom plan based on your findings.
 
-// Run the request/response through the application and emit the response.
-$server->emit($server->run());
+Need suggestions along the way? We’ll consult with you to brainstorm features you may never have considered otherwise. Exposed beams? Columns? A laundry chute? You’d be amazed at what we can do, and in the end you’ll be in a magazine-ready home, completely unique to you! The possibilities are endless.
+
+True North Builders LLC doesn’t just build homes, we build experiences. Through our commitment to you and your dream, we strive to exceed expectations and create a space for you and your family to live and grow in for years to come.
+
+We hope you are as excited as we are to speak to one another and start building!</h3>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="footer">
+                                        <div class="footer-wrapper">
+                                            <div class="grid-container fluid">
+                                                <div class="grid-x grid-margin-x">
+                                                    <div class="cell small-4 float-left">
+                                                    
+                                                    </div>
+                                                    <div class="cell medium-4 float-center">
+                                                        <h3 class="footer-text-center"><i class="fi-telephone"></i> <a href="tel:2088196630">(208) 819-6630</a></h3>
+                                                    </div>
+                                                    <div class="cell small-4 float-right">
+                                                        <h3 class="footer-text-right">Like us on Facebook <a href="https://www.facebook.com/Tnbuild/"><i class="fi-social-facebook"></i></a></h3>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+            <div class="reveal" id="quoteForm" data-reveal>
+                <button class="close-button" aria-label="Close form" type="button" data-close>
+                    <span aria-hidden="true">&times;</span>
+                </button>
+                <form>
+                    <div class="grid-container">
+                        <div class="grid-x grid-padding-x">
+                            <div class="medium-6 cell">
+                                <label>First Name
+                                    <input type="text" placeholder="First Name" />
+                                </label>
+                            </div>
+                            <div class="medium-6 cell">
+                                <label>
+                                    Last Name
+                                    <input type="text" placeholder="Last Name" />
+                                </label>
+                            </div>
+                            <div class="medium-12 cell">
+                                <label>
+                                    Phone Number
+                                    <input type="tel" placeholder="208-555-5555" />
+                                </label>
+                            </div>
+                            <div class="medium-12 cell">
+                                <label>
+                                    Email Address
+                                    <input type="email" placeholder="example@gmail.com" />
+                                </label>
+                            </div>
+                            <div class="medium-12 cell">
+                                <label>
+                                    Message
+                                    <textarea placeholder="Description" rows="5"></textarea>
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+		<script type="text/javascript" src="js/jquery.js"></script>
+                <script type="text/javascript" src="js/what-input.js"></script>
+                <script type="text/javascript" src="js/foundation.js"></script>
+                <script type="text/javascript" src="js/app.js"></script>
+                <script src="js/skrollr.js"></script>
+                <script src="js/main.js"></script>
+	</body>
+</html>	
---
+++ webroot/remodel.php
@@ -0,0 +1,146 @@
+<html class="no-js" lang="en" dir="ltr">
+    <head>
+        <title>True North Builders</title>
+	<meta charset="UTF-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+        <meta name="description" content="True North Builders" />
+        <meta name="keywords" content="jquery, css3, 3d, webkit, fallback, slider, css3, 3d transforms, slices, rotate, box, automatic" />
+	<meta name="author" content="Tyler Ludka" />
+	<link rel="shortcut icon" href="../favicon.ico"> 
+        <link rel="stylesheet" type="text/css" href="css/demo.css" />
+	<link rel="stylesheet" type="text/css" href="css/slicebox.css" />
+	<link rel="stylesheet" type="text/css" href="css/foundation.css" />
+        <link rel="stylesheet" type="text/css" href="css/styles.css" />
+	<link rel="stylesheet" type="text/css" href="css/custom.css" />
+        <link rel="stylesheet" href="css/app.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/motion-ui@1.2.3/dist/motion-ui.min.css" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+        <link rel="stylesheet" type="text/css" href="css/foundation-icons.css" />
+	<link rel="manifest" href="/site.webmanifest">
+        <link href='http://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
+        <link href="https://fonts.googleapis.com/css?family=Titillium+Web:400,200,300,700,600|Raleway:400,700,300" rel="stylesheet" type="text/css">
+	<script src="https://kit.fontawesome.com/a3270dcb61.js"></script>
+    </head>
+    <body>
+        <!-- Start Header -->
+        <div data-sticky-container data-sticky data-options="marginTop:0;" style="width:100%">
+            <div class="grid-container fluid header">
+                <div class="grid-x grid-margin-x align-middle">
+                    <div class="cell large-4">
+                        <h1><span>Coeur d'Alene Custom Home Builder, Home Remodeling and General Contractor</span></h1>
+                    </div>
+                    <div class="cell auto">
+                        <img src="images/logo_small.png" alt="True North Builders" />
+                    </div>
+                    <div class="cell auto">
+                        <h1><span>Located in Coeur d'Alene, serving Coeur d'Alene and surrounding areas</span></h1>
+                    </div>
+                </div>
+            </div>
+            <div class="title-bar" data-responsive-toggle="responsive-menu" data-hide-for="medium">
+                <button class="menu-icon" type="button" data-toggle="responsive-menu"></button>
+                <div class="title-bar-title">Menu</div>
+            </div>
+
+                
+            <div class="top-bar clearfix" id="responsive-menu" id="nav-menu">
+                <div class="top-bar-left">
+                    <ul class="dropdown menu align-middle" data-dropdown-menu>
+                        <li><a href="#home">Home</a>
+                        <li><a href="remodel.php">Remodeling</a></li>
+                        <li><a href="#contactus">Contact Us</a></li>
+                    </ul>
+                </div>
+                    
+                <div class="top-bar-right">
+                    <ul class="menu">
+                        <li><button type="button" class="button" data-open="quoteForm">Get a Quote</button></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <!-- End Header -->
+        <!-- Start Page Content -->
+        <div id="slide7">
+            <div class="slide-content">
+                <div id="rtt">
+                </div>
+            </div>
+        </div>
+        <div id="slide8">
+            <div class="slide-content">
+                <div class="slide-text-container">
+                    <h2>Remodeling: A True Investment</h2>
+                    <h3 class="slide-text">Making the decision to remodel part of, or your entire home, is an investment in your future. It may look daunting as you picture the project, but the benefits are worth it!
+
+Whether you’re expanding on space, providing maintenance-level renos to keep your home in flawless shape, or need to gut your property, all of these changes add to the curb appeal and overall value of your home.
+
+When you hire True North Builders LLC to embark on this journey with you, our team of trained professionals will make your home so shiny and new you’ll be wondering why you didn’t contact us sooner.</h3>
+                </div>
+            </div>
+        </div>
+        <div id="slide9">
+            <div class="slide-content">
+                <div id="sdp">
+                                                
+                </div>
+            </div>
+        </div>
+        <div id="slide10">
+            <div class="slide-content">
+                <div class="slide-text-container">
+                    <h2>Skill, Diligence and Professionalism</h2>
+                    <h3 class="slide-text">Our promise to you is this: skill, diligence and professionalism at all times. From our first meeting to our last day on the job you will be greeted with a smile, a handshake and an update on what is going on with your remodeling job.
+
+In our years of experience, the team at True North Builders LLC has been proud to work on hundreds of builds, whether it be decks, window replacement, small one-room facelifts or massive full-building renovations. We’ve done it all, and with each project we come out with more skills and knowledge to better serve you.
+
+Your contractor understands their job. They are not just a supervisor, but are accountable for their team and the results of a build. When you make the excellent choice to hire a contractor, they will do the managing so you don’t have to—troubleshooting when errors occur and enforcing safety precautions on the build site so no preventable injuries take place.</h3>
+                </div>
+            </div>
+        </div>
+        <div id="slide11">
+            <div class="slide-content">
+                <div id="gi"></div>
+            </div>
+        </div>
+        <div id="slide12">
+            <div class="slide-content">
+                <div class="slide-text-container">
+                    <h2>Remodeling for Home Value</h2>
+                    <h3 class="slide-text">At True North Builders LLC we love giving rooms and homes a fresh start. No one likes coming home to an old, tired space. Let us help you make the changes that will allow you to enjoy your home longer without having to start over, or prepare to sell your home without the concern you will not make a profit.</h3>
+                </div>
+            </div>
+        </div>
+        <!-- End Page Content -->
+        <!-- Start Footer -->
+        <div class="footer">
+            <div class="footer-wrapper">
+                <div class="grid-container fluid">
+                    <div class="grid-x grid-margin-x">
+                        <div class="cell small-4 float-left">
+                                                    
+                        </div>
+                        <div class="cell medium-4 float-center">
+                            <h3 class="footer-text-center"><i class="fi-telephone"></i> <a href="tel:2088196630">(208) 819-6630</a></h3>
+                        </div>
+                        <div class="cell small-4 float-right">
+                            <h3 class="footer-text-right">Like us on Facebook <a href="https://www.facebook.com/Tnbuild/"><i class="fi-social-facebook"></i></a></h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- End Footer -->
+        <!-- Begin Scripts -->
+        <script type="text/javascript" src="js/jquery.js"></script>
+        <script type="text/javascript" src="js/what-input.js"></script>
+        <script type="text/javascript" src="js/foundation.js"></script>
+        <script type="text/javascript" src="js/app.js"></script>
+        <script src="js/skrollr.js"></script>
+        <script src="js/main.js"></script>
+        <!-- End Scripts -->
+    </body>
+</html>
